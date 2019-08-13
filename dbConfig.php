de69b7ff5dcb4897cbcc682d3a4bd9d9c41ac4bb
silmisamuel <47229998+silmisamuel@users.noreply.github.com>
2019-08-13 19:50:20 +1200

    Update dbConfig.php

--- dbConfig.php
+++ dbConfig.php
@@ -1,8 +1,8 @@
 <?php
-define ("DB_HOST", "phmysqldb.mysql.database.azure.com"); // Your database host name
-define ("DB_USER", "prabaharanh@phmysqldb"); // Your database user
-define ("DB_PASS","Radhah@5969"); // Your database password
-define ("DB_NAME","dblogin"); // Your database name
+define ("DB_HOST", "silmiserver.mysql.database.azure.com"); // Your database host name
+define ("DB_USER", "sisa122@silmiserver"); // Your database user
+define ("DB_PASS","Ddyhajmou1"); // Your database password
+define ("DB_NAME","examdemo"); // Your database name
 
 $link = mysqli_connect(DB_HOST, DB_USER, DB_PASS) or die("Couldn't make connection.");
 $db = mysqli_select_db($link, DB_NAME) or die("Couldn't select database");
